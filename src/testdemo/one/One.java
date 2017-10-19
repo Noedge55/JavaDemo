d31fa906d2ee70688fda11eb94d581a06fe92eda
noedge55 <noedge@yeah.net>
2017-10-19 12:24:05 +0800

    给定一个正整数数组，随机取出一个数，要求数值越大的数被被取出的概率越大。

--- src/testdemo/one/One.java
+++ src/testdemo/one/One.java
@@ -2,7 +2,12 @@ package testdemo.one;
 
 import java.util.Arrays;
 import java.util.Random;
-
+/**
+ * 
+ * @author Noedge
+ *给定一个正整数数组，随机取出一个数，要求数值越大的数被被取出的概率越大。
+ *博客地址：http://blog.csdn.net/noedge/article/details/78282226
+ */
 public class One {
     public static void main(String[] args) {
         System.out.println(getItem(new int[]{4,2,3,1}));
