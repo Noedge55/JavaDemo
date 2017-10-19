7fe26208ab66e63b0021021661aa5992ed25e671
noedge55 <noedge@yeah.net>
2017-10-19 12:13:04 +0800

    javaDemo Project 初始化

---
+++ src/testdemo/one/One.java
@@ -0,0 +1,26 @@
+package testdemo.one;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class One {
+    public static void main(String[] args) {
+        System.out.println(getItem(new int[]{4,2,3,1}));
+    }
+    public static int getItem(int arr[]) {
+        Arrays.sort(arr);
+        int sum = 0 ;
+        for(int i = 1 ; i <= arr.length ; i++) {
+            sum += i;
+        }
+        int b[] = new int[sum];
+        for(int i = 0,index = 0 ; i < b.length ; index++,i += index) {
+            for(int j = i ; j < i + index + 1 ; j ++) {
+                b[j] = arr[index];
+            }
+        }
+        Random random = new Random();
+
+        return b[random.nextInt(sum)];
+    }
+}
